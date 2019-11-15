791a03233f5362724344ff5703761226530da5c7
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2019-11-15 13:45:15 -0600

    Add enlist_dots()

--- R/enlist_dots.R
+++ R/enlist_dots.R
@@ -1,3 +1,13 @@
+#' Enlist Dots
+#'
+#' Helper function to capture ... inputs from the user.
+#'
+#' @param ...
+#'
+#' @return list Returns a list for delayed evaluation in a function
+#' @export
+#'
+#' @examples
 enlist_dots <- function(...) {
   as.list(substitute(list(...)))[-1]
 }
