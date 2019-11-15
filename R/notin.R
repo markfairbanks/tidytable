6bcf5e797782c32b6fb9f2851a10089bed620e28
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2019-11-15 15:46:51 -0600

    Add %notin%

---
+++ R/notin.R
@@ -0,0 +1,13 @@
+#' notin operator
+#'
+#' @param x vector or NULL
+#' @param y vector or NULL
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' c(1,3,11) %notin% 1:10
+'%notin%' <- function(x,y){
+  !('%in%'(x,y))
+}
