05cee9331429c5c82f91ed05a71ab6b471678189
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2019-11-15 14:03:18 -0600

    Add rename

---
+++ R/rename.R
@@ -0,0 +1,30 @@
+#' Title
+#'
+#' @param .data
+#' @param ... Rename expression like dplyr::rename()
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' dt <- data.table(x = c(1,2,3))
+#' dt %>% rename(new_name = x)
+#'
+rename <- function(.data, ...) {
+
+  is.data.frame(.data) || is.data.table(.data) || stop(".data must be a data.table")
+
+  if (!is.data.table(.data)) {
+    .data = as.data.table(.data)
+  }
+
+  .dots <- enlist_dots(...)
+
+  for (i in seq_along(.dots)) {
+    new_name <- names(.dots)[[i]]
+    old_name <- as.character(.dots[[i]])
+
+    setnames(.data, old_name, new_name)
+  }
+  .data
+}
