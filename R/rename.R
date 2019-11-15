9da89a3c4e1357537813d2514560a3b2653d0f51
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2019-11-15 14:03:32 -0600

    Add rename

--- R/rename.R
+++ R/rename.R
@@ -7,8 +7,9 @@
 #' @export
 #'
 #' @examples
-#' dt <- data.table(x = c(1,2,3))
-#' dt %>% rename(new_name = x)
+#' dt <- data.table(x = c(1,2,3), y = c(4,5,6))
+#' dt %>% rename(new_x = x,
+#'               new_y = y)
 #'
 rename <- function(.data, ...) {
 
