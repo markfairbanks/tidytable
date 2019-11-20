2989c5492a525ef79fa7c3a193e19bd4d8e827bf
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2019-11-19 19:59:41 -0600

    Add dt_pivot_longer()

---
+++ R/pivot_longer.R
@@ -0,0 +1,41 @@
+#' Pivot data from wide to long
+#'
+#' `pivot_longer()` "lengthens" data, increasing the number of rows and
+#' decreasing the number of columns. The inverse transformation is
+#' [pivot_wider()]
+#'
+#' @param data
+#' @param cols Column selection. If empty uses all columns.
+#' @param names_to Name of the new "names" column. Must be a string.
+#' @param values_to Name of the new "values" column. Must be a string.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' example_df <- data.table(x = c(1,2,3), y = c(4,5,6), z = c("a", "b", "c"))
+#' example_df %>%
+#'   as_dt() %>%
+#'   dt_pivot_longer(cols = c(x, y), names_to = "stuff", values_to = "things")
+dt_pivot_longer <- function(data,
+                            cols,
+                            names_to = "name",
+                            values_to = "value") {
+  if (length(as.character(substitute(cols))[-1]) > 0) {
+    cols <- as.character(substitute(cols))[-1]
+  } else {
+    cols <- colnames(data)
+  }
+
+  id_vars <- colnames(data)[!colnames(data) %in% cols]
+
+  data.table::melt(data = data,
+                   id.vars = id_vars,
+                   measure.vars = cols,
+                   variable.name = names_to,
+                   value.name = values_to,
+                   # ...,
+                   na.rm = FALSE,
+                   variable.factor = FALSE,
+                   value.factor = FALSE)
+}
