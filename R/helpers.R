337b401b98b6b2b1ee51fe3e8bc56cd2f03b67a4
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2019-12-15 17:00:13 -0600

    Edit helpers

--- R/helpers.R
+++ R/helpers.R
@@ -20,15 +20,3 @@ characterize <- function(vec_list_expr) {
     as.character(vec_list_expr)[-1]
   }
 }
-
-insert_empty_i = function(expr){
-  call_list = as.list(expr)
-  call_list =  c(call_list[1:2], list(substitute()), call_list[-(1:2)])
-  as.call(call_list)
-}
-
-insert_empty_j <- function(expr) {
-  call_list = as.list(expr)
-  call_list = c(call_list[1:3], list(substitute()), call_list[-(1:3)])
-  as.call(call_list)
-}
