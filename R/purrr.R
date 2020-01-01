74256174cd14a0e7d07f2d8be0f411e63437082e
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-01 11:13:02 -0600

    Update purrr

--- R/purrr.R
+++ R/purrr.R
@@ -33,25 +33,25 @@ dt_map <- function(.x, .f, ...) {
 #' @export
 #' @inherit dt_map
 dt_map_lgl <- function(.x, .f, ...) {
-  setNames(vapply(.x, .f, logical(1), ..., USE.NAMES=FALSE), names(.x))
+  vapply(.x, .f, logical(1), ...)
 }
 
 #' @export
 #' @inherit dt_map
 dt_map_int <- function(.x, .f, ...) {
-  setNames(vapply(.x, .f, integer(1), ..., USE.NAMES=FALSE), names(.x))
+  vapply(.x, .f, integer(1), ...)
 }
 
 #' @export
 #' @inherit dt_map
 dt_map_dbl <- function(.x, .f, ...) {
-  setNames(vapply(.x, .f, double(1), ..., USE.NAMES=FALSE), names(.x))
+  vapply(.x, .f, double(1), ...)
 }
 
 #' @export
 #' @inherit dt_map
 dt_map_chr <- function(.x, .f, ...) {
-  setNames(vapply(.x, .f, character(1), ..., USE.NAMES=FALSE), names(.x))
+  vapply(.x, .f, character(1), ...)
 }
 
 #' @export
