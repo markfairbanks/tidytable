e00751d68bb11a230a0416eee1a67bfb9778913f
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-03 14:49:50 -0600

    Update column_selector()

--- R/helpers.R
+++ R/helpers.R
@@ -16,6 +16,7 @@ column_selector <- function(.data, select_vars) {
 
   select_index <- unlist(eval(select_vars, data_vars))
   keep_index <- unique(select_index[select_index > 0])
+  if (length(keep_index) == 0) keep_index <- seq_along(.data)
   drop_index <- unique(abs(select_index[select_index < 0]))
 
   select_index <- setdiff(keep_index, drop_index)
