9245d0791be44437dc0df68ad5bc219216a66131
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-10 07:19:34 -0600

    Fix error in dt_mutate_at()

--- R/helpers.R
+++ R/helpers.R
@@ -3,7 +3,7 @@ column_selector <- function(.data, select_vars) {
   data_names <- colnames(.data)
   data_vars <- setNames(as.list(seq_along(.data)), data_names)
 
-  select_index <- eval(select_vars, data_vars)
+  select_index <- unlist(eval(select_vars, data_vars))
 
   keep_index <- unique(select_index[select_index > 0])
   if (length(keep_index) == 0) keep_index <- seq_along(.data)
