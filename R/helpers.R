2310cb9e3b42f20c0df53459a5b0adb93f77b08c
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-14 20:27:11 -0600

    Update column_selector() to rlang

--- R/helpers.R
+++ R/helpers.R
@@ -1,9 +1,12 @@
 column_selector <- function(.data, select_vars) {
+  if (!is.data.frame(.data)) stop(".data must be a data.frame or data.table")
+  if (!is.data.table(.data)) .data <- as.data.table(.data)
 
   data_names <- colnames(.data)
   data_vars <- setNames(as.list(seq_along(.data)), data_names)
 
-  select_index <- unlist(eval(select_vars, data_vars))
+  select_vars <- enexpr(select_vars)
+  select_index <- eval(expr(c(!!select_vars)), data_vars)
 
   keep_index <- unique(select_index[select_index > 0])
   if (length(keep_index) == 0) keep_index <- seq_along(.data)
