2d2ce7a4322783fe637a4dc4a369daec1dfc9497
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-16 14:44:13 -0600

    Add dt_case_when()

---
+++ R/case_when.R
@@ -0,0 +1,64 @@
+#' Case when
+#'
+#' @description
+#' This function allows you to use multiple if/else statements in one call.
+#' Note that this function is called differently than `dplyr::case_when`! See examples
+#'
+#' @param ... Sequence of condition, value designations
+#' @param default Default value. Set to NA. Argument must be named.
+#'
+#' @return A vector
+#' @export
+#'
+#' @examples
+#' test_df <- data.table(a = 1:10,
+#'                       b = 11:20,
+#'                       c = c(rep("a", 6), rep("b", 4)),
+#'                       d = c(rep("a", 4), rep("b", 6)))
+#'
+#' test_df %>%
+#'   as_dt() %>%
+#'   dt_mutate(x = dt_case_when(b < 13, 3,
+#'                              a > 4, 2,
+#'                              default = 10))
+#' test_df %>%
+#'   as_dt() %>%
+#'   dt_mutate(x = dt_case_when(c == "a","a",
+#'                              default = d))
+dt_case_when <- function(..., default = NULL) {
+  dots <- enexprs(...)
+
+  index <- '+'(1, 1:length(dots)) %% 2
+
+  conditions <- dots[index == 0]
+  values <- dots[index == 1]
+
+  if (length(conditions) == 0) abort("No conditions supplied")
+  if (length(values) == 0) abort("No values supplied")
+
+  if (length(conditions) != length(values))
+    abort("The length of conditions does not equal the length values")
+
+  if (is.null(default)) {
+    na_class <- class(values[[1]])
+    if (na_class == "logical") {
+      default <- NA
+    } else if (na_class == "complex") {
+      default <- NA_complex_
+    } else if (na_class == "integer") {
+      default <- NA_integer_
+    } else if (na_class == "character") {
+      default <- NA_character_
+    } else {
+      default <- NA_real_
+    }
+  }
+
+  vals <- default
+
+  for (i in seq_along(conditions)) {
+    vals <- fifelse(eval(conditions[[i]], parent.frame()), eval(values[[i]], parent.frame()), vals)
+  }
+
+  vals
+}
---
+++ tests/testthat/test-case_when.R
@@ -0,0 +1,22 @@
+test_that("dt_case_when works", {
+
+  set.seed(843)
+  x <- rnorm(1e5)
+
+  cased <-
+    dt_case_when(
+      x < median(x), "low",
+      x >= median(x), "high",
+      is.na(x), "other"
+    )
+
+  cased2 <- dt_case_when(x > median(x), 1,
+                         default = x)
+
+  expect_named(table(cased), c("high", "low"))
+  expect_error(dt_case_when(x < median(x), "three",
+                            default = x))
+  expect_equal(head(cased),
+               c("low","low","low","low","low","high"))
+
+})
