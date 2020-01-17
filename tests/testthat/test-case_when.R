b058671daaea6e05e00509d5550525304caba00d
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-17 06:24:41 -0600

    Update tests

--- tests/testthat/test-case_when.R
+++ tests/testthat/test-case_when.R
@@ -1,20 +1,20 @@
-test_that("dt_case_when works", {
+test_that("dt_case works", {
 
   set.seed(843)
   x <- rnorm(1e5)
 
   cased <-
-    dt_case_when(
+    dt_case(
       x < median(x), "low",
       x >= median(x), "high",
       is.na(x), "other"
     )
 
-  cased2 <- dt_case_when(x > median(x), 1,
-                         default = x)
+  cased2 <- dt_case(x > median(x), 1,
+                    default = x)
 
   expect_named(table(cased), c("high", "low"))
-  expect_error(dt_case_when(x < median(x), "three",
+  expect_error(dt_case(x < median(x), "three",
                             default = x))
   expect_equal(head(cased),
                c("low","low","low","low","low","high"))
