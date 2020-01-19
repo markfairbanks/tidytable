0247db21cab651f783c36e5648683e499f18e586
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-18 20:04:13 -0600

    Add optional column selection to dt_distinct()

--- R/distinct.R
+++ R/distinct.R
@@ -4,6 +4,7 @@
 #' Simple alias to unique(). Retain only unique/distinct rows from an input df.
 #'
 #' @param .data A data.frame or data.table
+#' @param ... Columns to select before determining uniqueness. If omitted, will use all columns
 #'
 #' @import data.table
 #' @return A data.table
@@ -11,10 +12,18 @@
 #'
 #' @examples
 #' dt %>% dt_distinct()
-dt_distinct <- function(.data) {
+dt_distinct <- function(.data, ...) {
 
   if (!is.data.frame(.data)) stop(".data must be a data.frame or data.table")
   if (!is.data.table(.data)) .data <- as.data.table(.data)
 
-  unique(.data)
+  dots <- enexprs(...)
+
+  if (length(dots) == 0) {
+    unique(.data)
+  } else {
+    .data %>%
+      dt_select(!!!dots) %>%
+      unique()
+  }
 }
