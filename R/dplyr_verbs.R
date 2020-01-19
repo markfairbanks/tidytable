b52cff7cb944e8adfd6f83d2fcececa908e5cae3
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-19 11:01:29 -0600

    Update dt_select()

--- R/dplyr_verbs.R
+++ R/dplyr_verbs.R
@@ -132,7 +132,7 @@ dt_select <- function(.data, ...) {
     append(logical_cols)
 
   select_vars <- enexprs(...)
-  select_index <- eval(expr(c(!!!select_vars)), data_vars)
+  select_index <- unlist(eval(expr(c(!!!select_vars)), data_vars))
 
   keep_index <- unique(select_index[select_index > 0])
   if (length(keep_index) == 0) keep_index <- seq_along(.data)
