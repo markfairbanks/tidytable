38062f84fffcb51b19fa62e65ff8ef7466e8267c
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-21 12:42:00 -0600

    Add purrr tests

---
+++ tests/testthat/test-purrr.R
@@ -0,0 +1,35 @@
+test_that("anon function works dt_map()", {
+  anon_list <- dt_map(c(1,2,3), function(.x) .x + 1)
+  twiddle_list <- dt_map(c(1,2,3), ~.x + 1)
+
+  expect_equal(anon_list, twiddle_list)
+})
+
+test_that("anon function works dt_map_dbl()", {
+  anon_dbl <- dt_map_dbl(c(1,2,3), function(.x) .x + 1)
+  twiddle_dbl <- dt_map_dbl(c(1,2,3), ~.x + 1)
+
+  expect_equal(anon_dbl, twiddle_dbl)
+})
+
+test_that("anon function works dt_map_chr()", {
+  anon_chr <- dt_map_chr(c(1,2,3), as.character)
+  twiddle_chr <- dt_map_chr(c(1,2,3), ~as.character(.x))
+
+  expect_equal(anon_chr, twiddle_chr)
+})
+
+
+test_that("anon function works dt_map2()", {
+  anon_list <- dt_map2(c(1,2,3), c(1,2,3), function(.x,.y) .x + .y)
+  twiddle_list <- dt_map2(c(1,2,3), c(1,2,3), ~.x + .y)
+
+  expect_equal(anon_list, twiddle_list)
+})
+
+test_that("anon function works dt_map2_dbl()", {
+  anon_dbl <- dt_map2_dbl(c(1,2,3), c(1,2,3), function(.x,.y) .x + .y)
+  twiddle_dbl <- dt_map2_dbl(c(1,2,3), c(1,2,3), ~.x + .y)
+
+  expect_equal(anon_dbl, twiddle_dbl)
+})
