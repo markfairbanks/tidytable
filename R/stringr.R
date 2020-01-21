e6d0fd7ff03d2902ce44716f711c1ede1e69c40a
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-21 12:41:32 -0600

    Add stringr helpers

---
+++ R/stringr.R
@@ -0,0 +1,21 @@
+# These functions are for internal use only
+
+dt_str_replace <- function(string, pattern, replacement) {
+  sub(pattern, replacement, string)
+}
+
+dt_str_replace_all <- function(string, pattern, replacement) {
+  gsub(pattern, replacement, string)
+}
+
+dt_str_detect <- function(string, pattern) {
+  grepl(pattern, string)
+}
+
+dt_str_starts <- function(string, pattern) {
+  startsWith(string, pattern)
+}
+
+dt_str_ends <- function(string, pattern) {
+  endsWith(string, pattern)
+}
