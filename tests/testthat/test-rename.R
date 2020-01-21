fa780c8a6016c5a5803894acbce27e49855182bd
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-21 13:18:09 -0600

    Add tests

--- tests/testthat/test-rename.R
+++ tests/testthat/test-rename.R
@@ -46,3 +46,56 @@ test_that("rename_all() works for all variables", {
 
   expect_named(df, c("x_append", "y_append"))
 })
+
+# twiddle testing -----------------------------------
+test_that("rename_if() works with twiddle", {
+  df <- data.table(x = c(1,1,1), y = c(2,2,2), z = c("a", "a", "b"))
+  anon_df <- df %>%
+    as_dt() %>%
+    dt_rename_if(is.character, function(.x) paste0(.x, "_character"))
+  twiddle_df <- df %>%
+    as_dt() %>%
+    dt_rename_if(is.character, ~ paste0(.x, "_character"))
+
+  expect_named(twiddle_df, c("x","y","z_character"))
+  expect_equal(anon_df, twiddle_df)
+})
+
+test_that("rename_at() works with twiddle", {
+  df <- data.table(x_start = c(1,1,1), end_x = c(2,2,2), z = c("a", "a", "b"))
+  anon_df <- df %>%
+    as_dt() %>%
+    dt_rename_at(c(dt_starts_with("x")), ~ paste0(.x, "_append"))
+  twiddle_df <- df %>%
+    as_dt() %>%
+    dt_rename_at(c(dt_starts_with("x")), ~ paste0(.x, "_append"))
+
+  expect_named(twiddle_df, c("x_start_append", "end_x", "z"))
+  expect_equal(anon_df, twiddle_df)
+})
+
+test_that("rename_across() works with twiddle", {
+  df <- data.table(x_start = c(1,1,1), end_x = c(2,2,2), z = c("a", "a", "b"))
+  anon_df <- df %>%
+    as_dt() %>%
+    dt_rename_across(c(dt_starts_with("x")), ~ paste0(.x, "_append"))
+  twiddle_df <- df %>%
+    as_dt() %>%
+    dt_rename_across(c(dt_starts_with("x")), ~ paste0(.x, "_append"))
+
+  expect_equal(anon_df, twiddle_df)
+})
+
+test_that("rename_all() works with twiddle", {
+  df <- data.table(x = c(1,1,1), y = c(2,2,2))
+  anon_df <- df %>%
+    as_dt() %>%
+    dt_rename_all(function(.x) paste0(.x, "_append"))
+
+  twiddle_df <- df %>%
+    as_dt() %>%
+    dt_rename_all(~ paste0(.x, "_append"))
+
+  expect_named(twiddle_df, c("x_append", "y_append"))
+  expect_equal(anon_df, twiddle_df)
+})
