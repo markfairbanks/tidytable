239c5fdfa7763995630b88cf7526757d73f60983
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-28 12:59:25 -0800

    Update mutate_variants

--- R/mutate_variants.R
+++ R/mutate_variants.R
@@ -16,7 +16,8 @@
 #'
 #' @param .data A data.frame or data.table
 #' @param .cols vector `c()` of bare column names for `dt_mutate_across()` to use
-#' @param .funs Functions to pass
+#' @param .fun Function to pass to `mutate_if()`` or `mutate_all()``
+#' @param .funs Functions to pass to `dt_mutate_across()`
 #' @param ... Other arguments for the passed function
 #'
 #' @return A data.table
@@ -41,49 +42,18 @@
 #' example_dt %>%
 #'   as_dt() %>%
 #'   dt_mutate_across(c(x, y), list(new = ~ .x * 2))
-dt_mutate_if <- function(.data, .predicate, .fun, ...) {
+dt_mutate_if <- function(.data, .vars, .funs, ...) {
+  .vars <- enexpr(.vars)
 
-  if (!is.data.frame(.data)) stop(".data must be a data.frame or data.table")
-  if (!is.data.table(.data)) .data <- as.data.table(.data)
-
-  .cols <- colnames(.data)[dt_map_lgl(.data, .predicate)]
-
-  if (length(.cols) > 0) {
-    .data[, (.cols) := dt_map(.SD, .fun, ...), .SDcols = .cols][]
-  } else {
-    .data
-  }
+  dt_mutate_across(.data, !!.vars, .funs, ...)
 }
 
 #' @export
 #' @rdname dt_mutate_if
 dt_mutate_at <- function(.data, .vars, .funs, ...) {
+  .vars <- enexpr(.vars)
 
-  .cols <- enexpr(.vars)
-  .cols <- vec_selector(.data, !!.cols) %>%
-    as.character()
-
-  if (!is.list(.funs)) {
-    if (length(.cols) > 0) {
-      .data[, (.cols) := dt_map(.SD, .funs, ...), .SDcols = .cols][]
-    } else {
-      .data
-    }
-  } else {
-
-    if (!is_named(.funs)) abort("functions passed in a list must be named")
-
-    new_names <- names(.funs)
-    for (.col in .cols) {
-      for (i in seq_along(new_names)) {
-        new_name <- paste0(.col, "_", new_names[[i]])
-        old <- .col
-        user_function <- anon_x(.funs[[i]])
-        .data[, (new_name) := user_function(.data[[old]])][]
-      }
-    }
-  }
-  .data
+  dt_mutate_across(.data, !!.vars, .funs, ...)
 }
 
 #' @export
@@ -119,12 +89,8 @@ dt_mutate_across <- function(.data, .cols, .funs, ...) {
 
 #' @export
 #' @rdname dt_mutate_if
-dt_mutate_all <- function(.data, .fun, ...) {
+dt_mutate_all <- function(.data, .funs, ...) {
 
-  if (!is.data.frame(.data)) stop(".data must be a data.frame or data.table")
-  if (!is.data.table(.data)) .data <- as.data.table(.data)
+  dt_mutate_across(.data, dt_everything(), .funs, ...)
 
-  .cols <- colnames(.data)
-
-  .data[, (.cols) := dt_map(.SD, .fun, ...), .SDcols = .cols][]
 }
