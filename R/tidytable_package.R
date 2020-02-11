0b3ced1f759b8fd3ca4bacff7319da297e59149c
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-02-11 16:12:28 -0600

    importFrom cleanup

--- R/tidytable_package.R
+++ R/tidytable_package.R
@@ -1,25 +1,10 @@
 # Suppress R CMD check note
 #' @import data.table
-#' @importFrom data.table data.table
-#' @importFrom data.table melt
-#' @importFrom data.table dcast
-#' @importFrom rlang abort
-#' @importFrom rlang caller_env
-#' @importFrom rlang enexpr
-#' @importFrom rlang enexprs
-#' @importFrom rlang expr
-#' @importFrom rlang eval_tidy
-#' @importFrom rlang is_formula
-#' @importFrom rlang is_named
-#' @importFrom rlang parse_expr
-#' @importFrom rlang seq2
-#' @importFrom rlang sym
-#' @importFrom rlang syms
-#' @importFrom stats as.formula
-#' @importFrom stats na.omit
-#' @importFrom stats setNames
-#' @importFrom utils head
-#' @importFrom utils tail
+#' @importFrom data.table data.table melt dcast
+#' @importFrom rlang abort caller_env enexpr enexprs expr eval_tidy
+#' @importFrom rlang is_formula is_named parse_expr seq2 sym syms
+#' @importFrom stats as.formula na.omit setNames
+#' @importFrom utils head tail
 NULL
 
 globalVariables(c("data", ".","..select_vars", ".count", "na_index"))
