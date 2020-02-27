176ea1efa97db9ac7eadd63af5aff6c55fdb5599
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-02-27 14:27:40 -0600

    Cleanup imports

--- R/tidytable_package.R
+++ R/tidytable_package.R
@@ -1,7 +1,5 @@
 # Suppress R CMD check note
 #' @import data.table
-#' @importFrom data.table as.data.table copy data.table dcast is.data.table melt
-#' @importFrom data.table setattr setDT
 #' @importFrom rlang abort caller_env enexpr enexprs expr eval_tidy
 #' @importFrom rlang is_formula is_named missing_arg parse_expr seq2 squash sym syms
 #' @importFrom stats as.formula na.omit setNames
