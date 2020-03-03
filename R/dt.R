99428507e844c8ac161ef1bdca9f0c0fc1b7ef73
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-03-02 20:18:25 -0600

    Add s3 methods for dt()

--- R/dt.R
+++ R/dt.R
@@ -4,23 +4,8 @@
 #' Pipeable data.table call
 #'
 #' @param .data A data.frame or data.table
-#' @param i See ?data.table:::`[.data.table`
-#' @param j See ?data.table:::`[.data.table`
-#' @param by See ?data.table:::`[.data.table`
-#' @param keyby See ?data.table:::`[.data.table`
-#' @param with See ?data.table:::`[.data.table`
-#' @param nomatch See ?data.table:::`[.data.table`
-#' @param mult See ?data.table:::`[.data.table`
-#' @param roll See ?data.table:::`[.data.table`
-#' @param rollends See ?data.table:::`[.data.table`
-#' @param which See ?data.table:::`[.data.table`
-#' @param .SDcols See ?data.table:::`[.data.table`
-#' @param verbose See ?data.table:::`[.data.table`
-#' @param allow.cartesian See ?data.table:::`[.data.table`
-#' @param drop See ?data.table:::`[.data.table`
-#' @param on See ?data.table:::`[.data.table`
+#' @param ... Arguments passed to data.table call. See ?data.table:::`[.data.table`
 #'
-#' @export
 #' @examples
 #' example_dt <- data.table::data.table(
 #'   x = c(1,2,3),
@@ -30,41 +15,18 @@
 #' example_dt %>%
 #'   dt(, ':='(double_x = x * 2)) %>%
 #'   dt(order(-double_x))
-dt <- function(.data, i, j, by, keyby, with = TRUE,
-               nomatch = getOption("datatable.nomatch", NA),
-               mult = "all",
-               roll = FALSE,
-               rollends = if (roll=="nearest") c(TRUE,TRUE)
-               else if (roll>=0) c(FALSE,TRUE)
-               else c(TRUE,FALSE),
-               which = FALSE,
-               .SDcols,
-               verbose = getOption("datatable.verbose"),                   # default: FALSE
-               allow.cartesian = getOption("datatable.allow.cartesian"),   # default: FALSE
-               drop = NULL, on = NULL) {
+#' @export
+dt <- function(.data, ...) {
   UseMethod("dt")
 }
 
 #' @export
-dt.data.frame <- function(.data, i, j, by, keyby, with = TRUE,
-  nomatch = getOption("datatable.nomatch", NA),
-  mult = "all",
-  roll = FALSE,
-  rollends = if (roll=="nearest") c(TRUE,TRUE)
-  else if (roll>=0) c(FALSE,TRUE)
-  else c(TRUE,FALSE),
-  which = FALSE,
-  .SDcols,
-  verbose = getOption("datatable.verbose"),                   # default: FALSE
-  allow.cartesian = getOption("datatable.allow.cartesian"),   # default: FALSE
-  drop = NULL, on = NULL) {
+dt.data.frame <- function(.data, ...) {
+  .data <- as.data.table(.data)
+  dt(.data, ...)
+}
 
-  sys_call <- sys.call()
-
-  sys_call[[1]] <- as.symbol("[")
-
-  if (!is.data.table(.data))
-    sys_call[[2]] <- substitute(as.data.table(.data))
-
-  eval_tidy(sys_call, env = caller_env())
+#' @export
+dt.data.table <- function(.data, ...) {
+  .data[...]
 }
