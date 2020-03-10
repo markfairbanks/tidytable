71a53257bc4f4dd27321bcbf42da3838a12e9ba9
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-03-09 21:12:43 -0500

    Cleanup of selectors

--- R/utils-selectors.R
+++ R/utils-selectors.R
@@ -17,14 +17,15 @@ vec_selector <- function(.data, select_vars) {
 vec_selector_i <- function(.data, select_vars) {
 
   data_names <- colnames(.data)
-  data_vars <- setNames(as.list(seq_along(.data)), data_names)
+  data_index <- seq_along(data_names)
+  data_vars <- setNames(as.list(data_index), data_names)
 
-  integer_cols <- list(is.integer = seq_along(data_names)[dt_map_lgl(.data, is.integer)])
-  double_cols <- list(is.double = seq_along(data_names)[dt_map_lgl(.data, is.double)])
-  numeric_cols <- list(is.numeric = seq_along(data_names)[dt_map_lgl(.data, is.numeric)])
-  character_cols <- list(is.character = seq_along(data_names)[dt_map_lgl(.data, is.character)])
-  factor_cols <- list(is.factor = seq_along(data_names)[dt_map_lgl(.data, is.factor)])
-  logical_cols <- list(is.logical = seq_along(data_names)[dt_map_lgl(.data, is.logical)])
+  integer_cols <- list(is.integer = data_index[dt_map_lgl(.data, is.integer)])
+  double_cols <- list(is.double = data_index[dt_map_lgl(.data, is.double)])
+  numeric_cols <- list(is.numeric = data_index[dt_map_lgl(.data, is.numeric)])
+  character_cols <- list(is.character = data_index[dt_map_lgl(.data, is.character)])
+  factor_cols <- list(is.factor = data_index[dt_map_lgl(.data, is.factor)])
+  logical_cols <- list(is.logical = data_index[dt_map_lgl(.data, is.logical)])
 
   data_vars <- data_vars %>%
     append(integer_cols) %>%
@@ -38,10 +39,10 @@ vec_selector_i <- function(.data, select_vars) {
   select_index <- unlist(eval(expr(c(!!select_vars)), data_vars))
 
   keep_index <- unique(select_index[select_index > 0])
-  if (length(keep_index) == 0) keep_index <- seq_along(.data)
+  if (length(keep_index) == 0) keep_index <- data_index
   drop_index <- unique(abs(select_index[select_index < 0]))
 
-  select_index <- setdiff(keep_index, drop_index)
+  select_index <- keep_index[!keep_index %in% drop_index]
 
   select_index
 }
@@ -61,14 +62,15 @@ dots_selector <- function(.data, ...) {
 dots_selector_i <- function(.data, ...) {
 
   data_names <- colnames(.data)
-  data_vars <- setNames(as.list(seq_along(.data)), data_names)
+  data_index <- seq_along(data_names)
+  data_vars <- setNames(as.list(data_index), data_names)
 
-  integer_cols <- list(is.integer = seq_along(data_names)[dt_map_lgl(.data, is.integer)])
-  double_cols <- list(is.double = seq_along(data_names)[dt_map_lgl(.data, is.double)])
-  numeric_cols <- list(is.numeric = seq_along(data_names)[dt_map_lgl(.data, is.numeric)])
-  character_cols <- list(is.character = seq_along(data_names)[dt_map_lgl(.data, is.character)])
-  factor_cols <- list(is.factor = seq_along(data_names)[dt_map_lgl(.data, is.factor)])
-  logical_cols <- list(is.logical = seq_along(data_names)[dt_map_lgl(.data, is.logical)])
+  integer_cols <- list(is.integer = data_index[dt_map_lgl(.data, is.integer)])
+  double_cols <- list(is.double = data_index[dt_map_lgl(.data, is.double)])
+  numeric_cols <- list(is.numeric = data_index[dt_map_lgl(.data, is.numeric)])
+  character_cols <- list(is.character = data_index[dt_map_lgl(.data, is.character)])
+  factor_cols <- list(is.factor = data_index[dt_map_lgl(.data, is.factor)])
+  logical_cols <- list(is.logical = data_index[dt_map_lgl(.data, is.logical)])
 
   data_vars <- data_vars %>%
     append(integer_cols) %>%
@@ -82,10 +84,10 @@ dots_selector_i <- function(.data, ...) {
   select_index <- unlist(eval(expr(c(!!!select_vars)), data_vars))
 
   keep_index <- unique(select_index[select_index > 0])
-  if (length(keep_index) == 0) keep_index <- seq_along(.data)
+  if (length(keep_index) == 0) keep_index <- data_index
   drop_index <- unique(abs(select_index[select_index < 0]))
 
-  select_index <- setdiff(keep_index, drop_index)
+  select_index <- keep_index[!keep_index %in% drop_index]
 
   select_index
 }
