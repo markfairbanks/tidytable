1b9c9bac222426af2fb3422144270933eec19bdd
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-03-15 22:54:25 -0500

    Update docs

--- R/transmute.R
+++ R/transmute.R
@@ -1,14 +1,18 @@
 #' Add new variables and drop all others
 #'
+#' @description
+#' Unlike `dt_mutate()`, `dt_transmute()` keeps only the variables that you create
+#'
 #' @param .data A data.frame or data.table
 #' @param ... Columns to create/modify
 #' @param by Optional: `list()` of bare column names to group by
 #'
+#' @md
 #' @examples
 #' # transmute keeps only the variables you create
-#'
 #' mtcars %>%
 #'   dt_transmute(displ_l = disp / 61.0237)
+#'
 #' @export
 dt_transmute <- function(.data, ..., by = NULL) {
   UseMethod("dt_transmute")
