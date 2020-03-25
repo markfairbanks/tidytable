1e563dce5fc697ba1c522843071306d55d84ae5f
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-03-25 11:16:36 -0500

    Add n.()

---
+++ R/n.R
@@ -0,0 +1,24 @@
+#' Number of observations in each group
+#'
+#' @description
+#' Helper function that can be used to find counts by group.
+#'
+#' Can be used inside `summarize.()`, `mutate.()`, & `filter.()`
+#'
+#' @export
+#'
+#' @examples
+#' test_df <- data.table(
+#'   x = c(1,2,3),
+#'   y = c(4,5,6),
+#'   z = c("a","a","b"))
+#'
+#' test_df %>%
+#'   summarize.(count = n.(),
+#'              by = z)
+#'
+#' test_df %>%
+#'   mutate.(count = n.())
+n. <- function() {
+  eval_tidy(expr(.N), env = caller_env())
+}
