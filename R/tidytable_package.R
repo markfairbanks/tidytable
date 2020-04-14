319118bd2d5bb979b6c7574657844f31f5698586
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-04-14 12:56:08 -0500

    Add rlang::expr_text to imports

--- R/tidytable_package.R
+++ R/tidytable_package.R
@@ -1,7 +1,7 @@
 # Suppress R CMD check note
 #' @import data.table
 #' @importFrom methods as
-#' @importFrom rlang abort as_function caller_env enexpr enexprs expr eval_tidy have_name
+#' @importFrom rlang abort as_function caller_env enexpr enexprs expr expr_text eval_tidy have_name
 #' @importFrom rlang is_formula is_named is_null missing_arg parse_expr seq2 squash sym syms
 #' @importFrom rlang `%||%`
 #' @importFrom stats as.formula na.omit setNames
