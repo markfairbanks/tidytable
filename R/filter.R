db24736cd852eeb7b75643c4981dba64eb138810
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-05-18 20:49:53 -0700

    filter.() with quosures

--- R/filter.R
+++ R/filter.R
@@ -3,7 +3,7 @@
 #' @description
 #' Filters a dataset to choose rows where conditions are true.
 #'
-#' @param .data A data.frame or data.table
+#' @param .df A data.frame or data.table
 #' @param ... Conditions to filter by
 #' @param by Columns to group by if filtering with a summary function
 #'
@@ -20,37 +20,37 @@
 #'
 #' example_dt %>%
 #'   filter.(b <= mean(b), by = c)
-filter. <- function(.data, ..., by = NULL) {
+filter. <- function(.df, ..., by = NULL) {
   UseMethod("filter.")
 }
 
 #' @export
-filter..data.frame <- function(.data, ..., by = NULL) {
+filter..data.frame <- function(.df, ..., by = NULL) {
 
-  .data <- as_tidytable(.data)
+  .df <- as_tidytable(.df)
 
-  dots <- enexprs(...)
+  dots <- enquos(...)
 
-  by <- enexpr(by)
+  by <- enquo(by)
 
-  if (is.null(by)) {
+  if (quo_is_null(by)) {
     for (dot in dots) {
-      .data <- eval_expr(
-        .data[!!dot]
-      )
+      .df <- eval_tidy(quo(
+        .df[!!dot]
+      ), .df)
     }
   } else {
-    by <- vec_selector_by(.data, !!by)
+    by <- vec_selector_by(.df, !!by)
 
-    col_order <- names(.data)
+    col_order <- names(.df)
 
-    .data <- eval_expr(
-      .data[, .SD[Reduce('&', list(!!!dots))], by = !!by]
-    )
+    .df <- eval_quo(
+      .df[, eval_quo(.SD[Reduce('&', list(!!!dots))], .SD), by = !!by],
+      .df)
 
-    setcolorder(.data, col_order)
+    setcolorder(.df, col_order)
   }
-  .data
+  .df
 }
 
 #' @export
