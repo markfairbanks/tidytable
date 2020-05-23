dea532ac1fc1d9023e8340bdfae088d613fbaed2
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-05-23 11:37:27 -0700

    Fix group_split.() tests

--- tests/testthat/test-group_split.R
+++ tests/testthat/test-group_split.R
@@ -2,8 +2,8 @@ test_that("group_split.() keeps the grouping variables by default", {
   tbl <- tidytable(x = 1:4, g = factor(rep(c("a", "b"), each = 2)))
   res <- group_split.(tbl, g)
 
-  expect_equal(res, list(tbl[1:2,], tbl[3:4,]))
-  expect_identical(res, list(tbl[1:2,], tbl[3:4,]))
+  expect_equal(res[[1]], tbl[1:2,])
+  expect_equal(res[[2]], tbl[3:4,])
 })
 
 
@@ -11,5 +11,6 @@ test_that("group_split() can discard the grouping variables with .keep = FALSE",
   tbl <- tidytable(x = 1:4, g = factor(rep(c("a", "b"), each = 2)))
   res <- group_split.(tbl, g, .keep = FALSE)
 
-  expect_identical(res, list(tbl[1:2, 1, drop = FALSE], tbl[3:4,1, drop = FALSE]))
+  expect_equal(res[[1]], tbl[1:2, 1])
+  expect_equal(res[[2]], tbl[3:4,1])
 })
