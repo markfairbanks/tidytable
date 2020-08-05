7a4911745c0cf1bcf5009cf5dc22c16c4799202f
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-08-05 12:56:45 -0700

    Update lag/lead docs

--- R/lag_lead.R
+++ R/lag_lead.R
@@ -1,14 +1,12 @@
-#' Lead and Lag
+#' Get lagging or leading values
 #'
 #' @description
-#' Find the "next" or "previous" values in a vector.
-#' Useful for comparing values ahead of or behind the current values.
+#' Find the "previous" or "next" values in a vector.
+#' Useful for comparing values behind or ahead of the current values.
 #'
 #' @param x a vector of values
 #' @param n a positive integer of length 1, giving the number of positions to lead or lag by
 #' @param default value used for non-existent rows. Defaults to NA.
-#'
-#' @md
 #'
 #' @examples
 #' x <- 1:5
