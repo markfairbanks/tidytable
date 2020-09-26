b7ae9099f6668dbe310bd8b7e4236cee53b90309
Abdessabour Moutik <abds.papa@gmail.com>
2020-09-26 10:35:57 -0700

    Solve inconsistent console printing #139

--- R/print.R
+++ R/print.R
@@ -1,6 +1,6 @@
 #' @export
 print.tidytable <- function(x, ..., n = NULL, width = NULL, n_extra = NULL) {
-  if (knitr_installed && knitr_loaded) {
+  if (isTRUE(getOption('knitr.in.progress'))) {
 
     if (knitr::opts_chunk$get()$paged.print %||% TRUE)
       print(rmarkdown::paged_table(x))
@@ -17,9 +17,6 @@ print_mat <- function(x, n = NULL, width = NULL, n_extra = NULL) {
   mat$summary <- unname(mat$summary)
   print(mat)
 }
-
-knitr_installed <- rlang::is_installed("knitr")
-knitr_loaded <- "knitr" %in% loadedNamespaces()
 
 #' @export
 vec_ptype_abbr.tidytable <- function(x) {
