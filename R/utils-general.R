16a6058f53064a9ed4bcecd08bf7501bf4bb5880
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-11-30 22:28:51 -0700

    Add internal pmap

--- R/utils-general.R
+++ R/utils-general.R
@@ -1,4 +1,6 @@
 # Shortcut to use rlang quoting/unquoting with data.table/base R expressions
+# Currently used in `R/n.R` and `R/row_number.R`
+# Can be replaced by rlang::inject() if rlang dependency is bumped to v0.4.9
 eval_expr <- function(express) {
   eval_tidy(enexpr(express), env = caller_env())
 }
@@ -19,4 +21,27 @@ shallow <- function(x, cols = names(x), reset_class = FALSE) {
   else c("data.table", "data.frame")
   setattr(ans, 'class', class)
   ans[]
+}
+
+# For internal use only
+# Taken from: https://github.com/r-lib/rlang/blob/master/R/compat-purrr.R
+pmap. <- function(.l, .f, ...) {
+  .f <- as_function(.f)
+  args <- args_recycle(.l)
+  do.call("mapply", c(
+    FUN = list(quote(.f)),
+    args, MoreArgs = quote(list(...)),
+    SIMPLIFY = FALSE, USE.NAMES = FALSE
+  ))
+}
+
+args_recycle <- function(args) {
+  lengths <- map_int.(args, length)
+  n <- max(lengths)
+
+  stopifnot(all(lengths == 1L | lengths == n))
+  to_recycle <- lengths == 1L
+  args[to_recycle] <- map.(args[to_recycle], function(x) rep.int(x, n))
+
+  args
 }
