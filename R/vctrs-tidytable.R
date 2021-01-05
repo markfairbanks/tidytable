549612386e92b29874e127c53faf73ddb9822f0f
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-01-05 13:25:18 -0700

    Use exportS3Method. Closes #170

--- R/vctrs-tidytable.R
+++ R/vctrs-tidytable.R
@@ -16,47 +16,49 @@ tt_cast <- function(x, to, ...) {
   as_tidytable(df_cast(x, to, ...))
 }
 
-## vec_ptype2 -------------------------
-#' @export
+## vec_ptype2 ########################################
+
+#' @exportS3Method
 vec_ptype2.tidytable.tidytable <- function(x, y, ...) {
   tt_ptype2(x, y, ...)
 }
-#' @export
+#' @exportS3Method
 vec_ptype2.tidytable.data.frame <- function(x, y, ...) {
   tt_ptype2(x, y, ...)
 }
-#' @export
+#' @exportS3Method
 vec_ptype2.data.frame.tidytable <- function(x, y, ...) {
   tt_ptype2(x, y, ...)
 }
 
-#' @export
+#' @exportS3Method
 vec_ptype2.tidytable.tbl_df <- vec_ptype2.tidytable.data.frame
 
-#' @export
+#' @exportS3Method
 vec_ptype2.tbl_df.tidytable <- vec_ptype2.data.frame.tidytable
 
-## vec_cast -------------------------
-#' @export
+## vec_cast ########################################
+
+#' @exportS3Method
 vec_cast.tidytable.tidytable <- function(x, to, ...) {
   tt_cast(x, to, ...)
 }
-#' @export
+#' @exportS3Method
 vec_cast.tidytable.data.frame <- function(x, to, ...) {
   # `x` is a data.frame to be converted to a tidytable
   tt_cast(x, to, ...)
 }
-#' @export
+#' @exportS3Method
 vec_cast.data.frame.tidytable <- function(x, to, ...) {
   # `x` is a tidytable to be converted to a data.frame
   df_cast(x, to, ...)
 }
 
-#' @export
+#' @exportS3Method
 vec_cast.tbl_df.tidytable <- function(x, to, ...) {
   tib_cast(x, to, ...)
 }
 
-#' @export
+#' @exportS3Method
 vec_cast.tidytable.tbl_df <- vec_cast.tidytable.data.frame
 
