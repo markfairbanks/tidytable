77cd9d97dc0ddcfd2605a749aa835f947a374a03
Abdessabour Moutik <abds.papa@gmail.com>
2021-01-05 12:19:11 -0800

    Now raises error when number of groups mismatch column number

---
+++ R/extract.R
@@ -0,0 +1,58 @@
+extract. <- function(.df, ...) {
+  UseMethod("extract.")
+}
+
+extract..data.frame <- function(.df, col, into,
+                    regex = "([[:alnum:]]+)",
+                    remove = TRUE,
+                     ...) {
+  
+  .df <- as_tidytable(.df)
+  .df <- shallow(.df)
+
+  if (missing(col)) abort("col is missing and must be supplied")
+  if (missing(into)) abort("into is missing and must be supplied")
+
+  col <- select_vec_chr(.df, !!enquo(col))
+
+  groups <- str_extract_groups(.df[[col]], regex)
+
+  if (length(groups) != length(into)) {
+    abort(
+      glue("`regex` pattern should define {length(into)} groups; {length(groups)} found.")
+    )
+  }
+  .df[, (into) := groups]
+  
+  if (remove) .df[, (col) := NULL]
+
+  .df[]
+}
+
+str_extract_groups <- function(string, pattern){
+  groups <- regexpr(pattern, string, perl = TRUE)
+  start <- attr(groups, "capture.start")
+  end <- start + attr(groups, "capture.length") - 1L
+
+  # in order to force substr to return NA when No match is found
+  # set the start and end to NA
+  none_found <- start == -1 
+  start[none_found] <- NA
+  end[none_found] <- NA
+
+  map.(1:ncol(start), ~ substr(string, start[, .x], end[, .x]))
+}
+### making the test_df
+x <- paste0(LETTERS, letters, 1:28)[1:10]
+data.table(y = c(paste0(x, "-", rev(x)), 1:4)) -> test_df
+
+# extracting two columns
+test_df %>% extract.(y, c("A","B"), regex="([[:alnum:]]+)-([[:alnum:]]+)")
+
+# extracting only the first columns
+test_df %>% extract.(y, "A", regex="([[:alnum:]]+)")
+
+
+# extracting only the first columns
+test_df %>% extract.(y, "A")
+ test_df[, x:=y]
