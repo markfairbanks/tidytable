ee0f09a95d2288e4258f3def54eb79ff6ecba19c
Abdessabour Moutik <abds.papa@gmail.com>
2021-01-08 10:49:17 -0800

    Closes #168

--- R/extract.R
+++ R/extract.R
@@ -1,44 +1,79 @@
-extract. <- function(.df, ...) {
+#' Extract a character column into multiple columns using regex
+#' 
+#' @description 
+#' Given a regular expression with capturing groups, `extract()` turns each group
+#' into a new column. If the groups don't match, or the input is `NA`, the output
+#' will be `NA`.
+#'
+#' @param .df A data.table or data.frame
+#' @param col Column to extract from
+#' @param into New column names to split into. A character vector.
+#' @param regex A regular expression to extract the desired values. There
+#' should be one group (defined by `()`) for each element of `into`
+#' @param remove If TRUE, remove the input column from the output data.table
+#' @param convert If TRUE, runs `type.convert()` on the resulting column.
+#' Useful if the resulting column should be type integer/double.
+#' @param ... Additional arguments passed on to methods.
+#'
+#' @export
+#'
+#' @examples
+#' df <- data.table(x = c(NA, "a-b", "a-d", "b-c", "d-e"))
+#' df %>% extract.(x, "A")
+#' df %>% extract.(x, c("A", "B"), "([[:alnum:]]+)-([[:alnum:]]+)")
+#'
+#' # If no match, NA:
+#' df %>% extract.(x, c("A", "B"), "([a-d]+)-([a-d]+)")
+extract. <- function(.df, col, into, regex = "([[:alnum:]]+)",
+                     remove = TRUE, convert = FALSE, ...) {
   UseMethod("extract.")
 }
 
-extract..data.frame <- function(.df, col, into,
-                    regex = "([[:alnum:]]+)",
-                    remove = TRUE,
-                     ...) {
+#' @export
+extract..data.frame <- function(.df, col, into, regex = "([[:alnum:]]+)",
+                                remove = TRUE, convert = FALSE, ...) {
   
   .df <- as_tidytable(.df)
   .df <- shallow(.df)
-
+  
   if (missing(col)) abort("col is missing and must be supplied")
   if (missing(into)) abort("into is missing and must be supplied")
-
-  col <- select_vec_chr(.df, !!enquo(col))
-
-  groups <- str_extract_groups(.df[[col]], regex)
-
+  
+  col <- select_vec_idx(.df, {{ col }})
+  
+  groups <- str_extract_groups(.df[[col]], regex, convert)
+  
   if (length(groups) != length(into)) {
     abort(
       glue("`regex` pattern should define {length(into)} groups; {length(groups)} found.")
     )
   }
-  .df[, (into) := groups]
+  
+  .df[, (into) := ..groups]
   
   if (remove) .df[, (col) := NULL]
-
+  
   .df[]
 }
 
-str_extract_groups <- function(string, pattern){
+str_extract_groups <- function(string, pattern, convert){
   groups <- regexpr(pattern, string, perl = TRUE)
   start <- attr(groups, "capture.start")
   end <- start + attr(groups, "capture.length") - 1L
-
+  
   # in order to force substr to return NA when No match is found
   # set the start and end to NA
   none_found <- start == -1 
   start[none_found] <- NA
   end[none_found] <- NA
+  
+  lapply(
+    seq_len(ncol(start)),
+    function(.x) type_convert(substr(string, start[, .x], end[, .x]))
+  )
+}
 
-  map.(1:ncol(start), ~ substr(string, start[, .x], end[, .x]))
+type_convert <- function(x, .convert = convert) {
+    if (.convert) type.convert(x, as.is = TRUE)
+    else x
 }
