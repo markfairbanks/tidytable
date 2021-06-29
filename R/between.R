3d8a10534fac2de0bf047851ab48149c6c4f8ac7
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-06-29 16:05:18 -0700

    Update docs

--- R/between.R
+++ R/between.R
@@ -3,26 +3,27 @@
 #' @description
 #' `between.()` utilizes `data.table::between()` in the background
 #'
-#' @param x A numeric vector of values
+#' @param x A numeric vector
 #' @param left,right Boundary values
 #'
 #' @export
 #'
 #' @examples
-#' between.(1:10, 5, 7)
-#'
 #' test_df <- data.table(
-#'   x = sample(1:5, 10, replace = TRUE),
-#'   y = sample(1:5, 10, replace = TRUE)
+#'   x = 1:5,
+#'   y = 1:5
 #' )
 #'
 #' # Typically used in a filter.()
 #' test_df %>%
-#'   filter.(between.(x, 1,3))
+#'   filter.(between.(x, 2, 4))
+#'
+#' test_df %>%
+#'   filter.(x %>% between.(2, 4))
 #'
 #' # Can also use the %between% operator
 #' test_df %>%
-#'   filter.(x %between% c(1, 3))
+#'   filter.(x %between% c(2, 4))
 between. <- function(x, left, right) {
   between(x = x, lower = left, upper = right)
 }
