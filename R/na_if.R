36fb6f40bfb30496f40617d5ad70cbb638500f53
markfairbanks <mark.t.fairbanks@gmail.com>
2022-05-24 09:53:05 -0600

    Implement `na_if`

---
+++ R/na_if.R
@@ -0,0 +1,20 @@
+#' Convert values to NA
+#'
+#' @description
+#' Convert values to `NA`.
+#'
+#' @param x An R object
+#' @param y Value(s) to replace with `NA`
+#'
+#' @export
+#'
+#' @examples
+#' vec <- 1:3
+#' na_if.(vec, 3)
+na_if. <- function(x, y) {
+  if (length(y) == 1) {
+    vec_assign(x, x == y, NA)
+  } else {
+    vec_assign(x, x %f_in% y, NA)
+  }
+}
