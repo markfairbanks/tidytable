821e199bb17d7c89b390c5e27dbdb5908d3f23b0
markfairbanks <mark.t.fairbanks@gmail.com>
2022-07-05 09:56:28 -0600

    Add `.default`/`.ptype`/`.size` args

--- R/case_when.R
+++ R/case_when.R
@@ -7,7 +7,10 @@
 #' in the background for improved performance.
 #'
 #' @param ... A sequence of two-sided formulas. The left hand side gives the conditions,
-#' the right hand side gives the values.
+#'   the right hand side gives the values.
+#' @param .default The default value if all conditions evaluate to `FALSE`.
+#' @param .ptype Optional ptype to specify the output type.
+#' @param .size Optional size to specify the output size.
 #'
 #' @export
 #'
@@ -18,29 +21,36 @@
 #'   mutate.(case_x = case_when.(x < 5 ~ 1,
 #'                               x < 7 ~ 2,
 #'                               TRUE ~ 3))
-case_when. <- function(...) {
+case_when. <- function(..., .default = NA, .ptype = NULL, .size = NULL) {
   dots <- list2(...)
   dots_length <- length(dots)
   if (dots_length == 0) abort("No cases provided.")
 
-  # Extract default value
-  default_bool <- map_lgl.(dots, ~ is_true(f_lhs(.x)))
+  is_default <- map_lgl.(dots, ~ is_true(f_lhs(.x)))
+  if (any(is_default) && dots_length > 1) {
+    .default <- dots[is_default][[1]]
+    .default <- eval_tidy(f_rhs(.default), env = caller_env())
 
-  if (all(default_bool == FALSE)) {
-    default_val <- NA
-  } else {
-    default_val <- dots[default_bool][[1]]
-    default_val <- eval_tidy(f_rhs(default_val), env = caller_env())
+    dots <- dots[!is_default]
   }
 
-  # Remove default value from queries
-  dots <- dots[!default_bool]
-
   conditions <- map.(dots, f_lhs)
+  conditions <- map.(conditions, eval_tidy, env = caller_env())
+
   values <- map.(dots, f_rhs)
+  values <- map.(values, eval_tidy, env = caller_env())
+
+  if (!is.null(.ptype)) {
+    values <- vec_cast_common(!!!values, .to = .ptype)
+  }
 
   pairs <- vec_interleave(conditions, values)
-  pairs <- map.(pairs, eval_tidy, env = caller_env())
 
-  case.(!!!pairs, default = default_val)
+  out <- case.(!!!pairs, default = .default)
+
+  if (!is.null(.size)) {
+    out <- vec_recycle(out, .size)
+  }
+
+  out
 }
