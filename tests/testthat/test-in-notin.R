9b166ad5dba7ca8962090831d889beb0cda0aa89
markfairbanks <mark.t.fairbanks@gmail.com>
2022-08-21 08:46:10 -0600

    Add test

--- tests/testthat/test-in-notin.R
+++ tests/testthat/test-in-notin.R
@@ -1,11 +1,19 @@
+# %in% ----------------------------------------------------
+
 test_that("%in% works", {
   expect_equal(c("a", "d") %in% c("a", "b"), c(TRUE, FALSE))
 })
 
-test_that("%notin% works", {
-  expect_equal(c("a", "d") %notin% c("a", "b"), c(FALSE, TRUE))
-})
-
 test_that("properly handles character comparison with NA", {
   expect_equal(c("a", "b") %in% NA, c(FALSE, FALSE))
+})
+
+test_that("can compare to a list, #565", {
+  expect_equal(c(1, 2) %in% list(1), c(TRUE, FALSE))
+})
+
+# %notin% ----------------------------------------------------
+
+test_that("%notin% works", {
+  expect_equal(c("a", "d") %notin% c("a", "b"), c(FALSE, TRUE))
 })
