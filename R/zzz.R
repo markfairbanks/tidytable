f5de41dabf72a1e16fcad1bdc992a0e137ebea3d
markfairbanks <mark.t.fairbanks@gmail.com>
2022-08-26 15:24:48 -0600

    Add onLoad message

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function(...) {
+  msg <- glue(
+    "As of tidytable v0.9.0, dotless versions of functions are exported.
+    You can now use `arrange()`/`mutate()`/etc. directly.
+    The `verb.()` versions will remain in the package."
+  )
+  message(msg)
+}
