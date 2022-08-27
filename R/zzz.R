4f8109551d3dce5f813f5ee95058071cb846fd99
markfairbanks <mark.t.fairbanks@gmail.com>
2022-08-27 13:10:46 -0600

    Use `pacakgeStartupMessage`

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
-.onLoad <- function(...) {
-  msg <- glue(
+.onAttach <- function(...) {
+  msg <- glue::glue(
     "As of tidytable v0.9.0, dotless versions of functions are exported.
     You can now use `arrange()`/`mutate()`/etc. directly.
     Note: The `verb.()` versions will remain in the package."
   )
-  message(msg)
+  packageStartupMessage(msg)
 }
