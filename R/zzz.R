1651b8658841f746857cc0962e1dd6b9cced5923
markfairbanks <mark.t.fairbanks@gmail.com>
2022-09-12 13:58:21 -0700

    Clean up startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,7 @@
 .onAttach <- function(...) {
   msg <- glue::glue(
-    "As of tidytable v0.9.0, dotless versions of functions are exported.
-    You can now use `arrange()`/`mutate()`/etc. directly.
-    Note: The `verb.()` versions will remain in the package."
+    "As of tidytable v0.9.0 dotless versions of functions are exported.
+    You can now use `arrange()`/`mutate()`/etc. directly."
   )
   packageStartupMessage(msg)
 }
