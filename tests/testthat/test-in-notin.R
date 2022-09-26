230a49bbc6dceb75357092e61bdff38cde797f23
krterberg <35448891+krterberg@users.noreply.github.com>
2022-09-26 18:49:05 +0200

    Update tests for %in% to use internal version

--- tests/testthat/test-in-notin.R
+++ tests/testthat/test-in-notin.R
@@ -1,15 +1,15 @@
 # %in% ----------------------------------------------------
 
-test_that("%in% works", {
-  expect_equal(c("a", "d") %in% c("a", "b"), c(TRUE, FALSE))
+test_that("%in.% works", {
+  expect_equal(c("a", "d") %in.% c("a", "b"), c(TRUE, FALSE))
 })
 
 test_that("properly handles character comparison with NA", {
-  expect_equal(c("a", "b") %in% NA, c(FALSE, FALSE))
+  expect_equal(c("a", "b") %in.% NA, c(FALSE, FALSE))
 })
 
 test_that("can compare to a list, #565", {
-  expect_equal(c(1, 2) %in% list(1), c(TRUE, FALSE))
+  expect_equal(c(1, 2) %in.% list(1), c(TRUE, FALSE))
 })
 
 # %notin% ----------------------------------------------------
