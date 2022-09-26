54b936003cf3b849250bd5c8b426d7ada0cb5fd4
krterberg <github@terberg.dev>
2022-09-27 01:38:23 +0200

    Typo fix

--- tests/testthat/test-in-notin.R
+++ tests/testthat/test-in-notin.R
@@ -4,7 +4,7 @@ test_that("%in% works", {
   expect_equal(c("a", "d") %in% c("a", "b"), c(TRUE, FALSE))
 })
 
-test_that("%in% falls back to base implementatrion on arguments that don't share a prototype", {
+test_that("%in% falls back to base implementation on arguments that don't share a prototype", {
   expect_equal(c("1", "4") %in% c(1, 2), c(TRUE, FALSE))
 })
 
