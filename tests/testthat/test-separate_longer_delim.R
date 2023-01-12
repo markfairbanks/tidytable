43c182199309a6c228011276d3358e2a6771bdcd
markfairbanks <mark.t.fairbanks@gmail.com>
2023-01-12 09:02:40 -0700

    Update tests

--- tests/testthat/test-separate_longer_delim.R
+++ tests/testthat/test-separate_longer_delim.R
@@ -11,8 +11,8 @@ test_that("default pattern does not split decimals in nested strings", {
   expect_equal(result$y, c("1", "1.0", "1.1", "2.1"))
 })
 
-test_that("can multiple columns", {
-  df <- tidytable(id = 1:3, x = c("x", "x y", "x y z"), y = c("a", "a b", "a b c"))
+test_that("can split multiple columns & works on data.frame input", {
+  df <- data.frame(id = 1:3, x = c("x", "x y", "x y z"), y = c("a", "a b", "a b c"))
   result <- df %>%
     separate_longer_delim(c(x, y), delim = " ")
   expect_equal(result$id, c(1, 2, 2, 3, 3, 3))
