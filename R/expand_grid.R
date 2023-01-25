5ebe523771be824394b79108d95a36d9280d3842
markfairbanks <mark.t.fairbanks@gmail.com>
2023-01-25 10:53:09 -0700

    Use `vec_expand_grid()`

--- R/expand_grid.R
+++ R/expand_grid.R
@@ -18,55 +18,19 @@
 expand_grid <- function(..., .name_repair = "check_unique") {
   dots <- dots_list(..., .named = TRUE)
 
-  if (any(map_lgl(dots, is.data.frame))) {
-    df_expand_grid(!!!dots, .name_repair = .name_repair)
-  } else {
-    cj_expand_grid(!!!dots, .name_repair = .name_repair)
-  }
+  is_data_frame <- map_lgl(dots, is.data.frame)
+
+  out <- vec_expand_grid(!!!dots, .name_repair = "minimal")
+
+  # df_list requires data frames inputs to be unnamed to unpack
+  out <- as.list(out)
+  names(out)[is_data_frame] <- ""
+  out <- df_list(!!!out, .name_repair = .name_repair)
+
+  new_tidytable(out)
 }
 
 #' @export
 #' @keywords internal
 #' @inherit expand_grid
 expand_grid. <- expand_grid
-
-cj_expand_grid <- function(..., .name_repair = "check_unique") {
-  out <- exec(CJ, ..., unique = FALSE, sorted = FALSE)
-
-  out <- df_name_repair(out, .name_repair)
-
-  as_tidytable(out)
-}
-
-df_expand_grid <- function(..., .name_repair = "check_unique") {
-  l <- list2(...)
-
-  sizes <- list_sizes(l)
-
-  size <- prod(sizes)
-
-  if (size == 0) {
-    out <- map(l, vec_slice, integer())
-  } else {
-    each <- size / cumprod(sizes)
-    times <- size / each / sizes
-    l_names <- names(l) %||% as.character(seq_along(l))
-
-    out <- pmap(
-      list(x = l, each = each, times = times, x_name = l_names),
-      make_cj_tidytable
-    )
-  }
-
-  out <- df_list(!!!out, .name_repair = .name_repair)
-  new_tidytable(out)
-}
-
-make_cj_tidytable <- function(x, each, times, x_name) {
-  out <- vec_rep(vec_rep_each(x, each), times = times)
-  if (!is.data.frame(out)) {
-    # If `out` is a vector make it a tidytable
-    out <- tidytable(!!x_name := out)
-  }
-  out
-}
