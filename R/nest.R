f346bcb25fdc4c47846aded6a3445bf07a505801
markfairbanks <mark.t.fairbanks@gmail.com>
2023-02-01 13:11:24 -0700

    Implement `.by` and `.key` args

--- R/nest.R
+++ R/nest.R
@@ -1,22 +1,24 @@
-#' Nest data.tables
+#' Nest columns into a list-column
 #'
 #' @description
-#' Nest data.tables
+#' Nest columns into a list-column
 #'
 #' @param .df A data.table or data.frame
 #' @param ... Columns to be nested.
+#' @param .by Columns to nest by
+#' @param .key New column name if `.by` is used
 #' @param .names_sep If NULL, the names will be left alone. If a string,
-#' the names of the columns will be created by pasting together the inner
-#' column names and the outer column names.
+#'   the names of the columns will be created by pasting together the inner
+#'   column names and the outer column names.
 #'
 #' @export
 #'
 #' @examples
 #' df <- data.table(
-#'   a = 1:10,
-#'   b = 11:20,
-#'   c = c(rep("a", 6), rep("b", 4)),
-#'   d = c(rep("a", 4), rep("b", 6))
+#'   a = 1:3,
+#'   b = 1:3,
+#'   c = c("a", "a", "b"),
+#'   c = c("a", "a", "b")
 #' )
 #'
 #' df %>%
@@ -24,19 +26,22 @@
 #'
 #' df %>%
 #'   nest(data = where(is.numeric))
-nest <- function(.df, ..., .names_sep = NULL) {
-  nest.(.df, ..., .names_sep = .names_sep)
+#'
+#' df %>%
+#'   nest(.by = c(c, d))
+nest <- function(.df, ..., .by = NULL, .key = NULL, .names_sep = NULL) {
+  nest.(.df, ..., .by = {{ .by }}, .key = .key, .names_sep = .names_sep)
 }
 
 #' @export
 #' @keywords internal
 #' @inherit nest
-nest. <- function(.df, ..., .names_sep = NULL) {
+nest. <- function(.df, ..., .by = NULL, .key = NULL, .names_sep = NULL) {
   UseMethod("nest.")
 }
 
 #' @export
-nest..tidytable <- function(.df, ..., .names_sep = NULL) {
+nest..tidytable <- function(.df, ..., .by = NULL, .key = NULL, .names_sep = NULL) {
   if (!is.null(.names_sep)) {
     vec_assert(.names_sep, character(), 1)
   }
@@ -51,34 +56,54 @@ nest..tidytable <- function(.df, ..., .names_sep = NULL) {
     abort("All elements of `...` must be named. For example `nest(data = c(x, y))`")
   }
 
-  .key <- names(dots)
+  .by <- enquo(.by)
 
-  dots <- unname(dots)
+  no_dots <- length(dots) == 0
 
-  if (!is.null(.names_sep)) {
-    nest_cols <- tidyselect_names(.df, !!!dots)
+  null_by <- quo_is_null(.by)
 
-    new_names <- paste(.key, nest_cols, sep = .names_sep)
-
-    .df <- df_set_names(.df, new_names, nest_cols)
-
-    dots <- syms(new_names)
+  if (no_dots) {
+    .key <- .key %||% "data"
+  } else {
+    .key <- names(dots)
   }
 
-  nest_by(.df, -c(!!!dots), .key = .key)
+  if (no_dots && null_by) {
+    .by <- character()
+    cols <- names(.df)
+  } else if (no_dots) {
+    .by <- tidyselect_names(.df, !!.by)
+    cols <- tidyselect_names(.df, -any_of(.by))
+  } else if (null_by) {
+    cols <- tidyselect_names(.df, !!dots[[1]])
+    .by <- tidyselect_names(.df, -any_of(cols))
+  } else {
+    .by <- tidyselect_names(.df, !!.by)
+    cols <- tidyselect_names(.df, !!dots[[1]])
+  }
+
+  if (!is.null(.names_sep)) {
+    new_names <- paste(.key, cols, sep = .names_sep)
+
+    .df <- df_set_names(.df, new_names, cols)
+
+    cols <- new_names
+  }
+
+  dt_j(.df, .(!!.key := list(.SD)), by = .by, .SDcols = cols)
 }
 
 #' @export
-nest..grouped_tt <- function(.df, ..., .names_sep = NULL) {
+nest..grouped_tt <- function(.df, ..., .by = NULL, .key = NULL, .names_sep = NULL) {
   .groups <- group_vars(.df)
   out <- ungroup(.df)
-  out <- nest(out, data = -all_of(.groups), .names_sep = .names_sep)
+  out <- nest(out, ..., .by = any_of(.groups), .key = .key, .names_sep = .names_sep)
   group_by(out, all_of(.groups))
 }
 
 #' @export
-nest..data.frame <- function(.df, ..., .names_sep = NULL) {
+nest..data.frame <- function(.df, ..., .by = NULL, .key = NULL, .names_sep = NULL) {
   .df <- as_tidytable(.df)
-  nest.(.df, ..., .names_sep = .names_sep)
+  nest.(.df, ..., .by = {{ .by }}, .key = .key, .names_sep = .names_sep)
 }
 
