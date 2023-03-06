3290c509c639b4c0662e81e129e01d814e6f19a5
markfairbanks <mark.t.fairbanks@gmail.com>
2023-03-06 13:37:13 -0700

    Simplify and improve performance

--- R/utils-fast_copy.R
+++ R/utils-fast_copy.R
@@ -1,36 +1,24 @@
 # Fast copying
-# Deep copy of columns that are being overwritten.
+#   Deep copy of columns that are being overwritten.
 #   Only does a shallow copy of the other columns.
 #   Faster than running `copy()` on an entire data frame.
-fast_copy <- function(x, new_cols = character()) {
-  if (length(new_cols) == 0) {
+fast_copy <- function(x, cols = character()) {
+  if (length(cols) == 0) {
     return(shallow(x))
   }
 
-  x_names <- names(x)
-
-  needs_copy <- x_names %in% new_cols
+  needs_copy <- names(x) %in% cols
 
   if (any(needs_copy)) {
-    x_names <- copy(x_names)
-    out <- vector("list", length(x_names))
-    setattr(out, "names", x_names)
-    for (i in seq_along(x_names)) {
-      col <- x_names[[i]]
-      if (needs_copy[[i]]) {
-        out[[col]] <- copy(x[[col]])
-      } else {
-        out[[col]] <- x[[col]]
-      }
-    }
-    setDT(out)
-    class <- copy(class(x))
-    setattr(out, "class", class)
+    out <- as.list(x)
+    out[needs_copy] <- copy(out[needs_copy])
+    out <- new_tidytable(out)
+    out <- tidytable_restore(out, x)
   } else {
-    out <- shallow(x)
+    out <- x
   }
 
-  out
+  shallow(out)
 }
 
 # Creates a shallow copy
@@ -39,16 +27,3 @@ shallow <- function(x) {
   x[TRUE]
 }
 
-# deprecated shallow() ------------------------------------------------
-# shallow <- function(x, cols = names(x), reset_class = FALSE) {
-#   stopifnot(is.data.table(x), all(cols %in% names(x)))
-#   ans <- vector("list", length(cols))
-#   setattr(ans, 'names', copy(cols))
-#   for (col in cols) {
-#     ans[[col]] = x[[col]]
-#   }
-#   setDT(ans)
-#   class  <- if (!reset_class) copy(class(x)) else c("data.table", "data.frame")
-#   setattr(ans, 'class', class)
-#   ans[]
-# }
