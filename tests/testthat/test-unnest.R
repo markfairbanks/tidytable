97993ac93ec6200dbc9a07f3f9941496309fd403
markfairbanks <mark.t.fairbanks@gmail.com>
2023-11-28 19:04:27 -0700

    Add test for length 0 vectors

--- tests/testthat/test-unnest.R
+++ tests/testthat/test-unnest.R
@@ -132,13 +132,13 @@ test_that("works when the only column is a list column", {
   expect_equal(result_df$x, rep(1:3, 3))
 })
 
-test_that("keep_empty", {
+test_that("keep_empty works with NULL and length 0 vectors, #783", {
   nest_df <- tidytable(x = 1, y = 2)
 
   test_df <- tidytable(
     groups = c("a", "b", "c"),
     df_list = list(NULL, nest_df, nest_df),
-    vecs = list(NULL, 1, 2)
+    vecs = list(double(), 1, 2)
 
   )
 
