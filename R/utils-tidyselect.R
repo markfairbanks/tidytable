abbaaf3dcfed13213b7f2511ce74eeb4d7050988
markfairbanks <mark.t.fairbanks@gmail.com>
2024-02-12 10:45:24 -0700

    Add `.allow_rename` arg

--- R/utils-tidyselect.R
+++ R/utils-tidyselect.R
@@ -2,14 +2,14 @@
 # _names: Get column names (as a character vector)
 # _syms: Get column names (as a list of symbols)
 
-tidyselect_locs <- function(.df, ...) {
-  eval_select(expr(c(...)), .df)
+tidyselect_locs <- function(.df, ..., .allow_rename = TRUE) {
+  eval_select(expr(c(...)), .df, allow_rename = .allow_rename)
 }
 
-tidyselect_names <- function(.df, ...) {
-  names(tidyselect_locs(.df, ...))
+tidyselect_names <- function(.df, ..., .allow_rename = TRUE) {
+  names(tidyselect_locs(.df, ..., .allow_rename = .allow_rename))
 }
 
-tidyselect_syms <- function(.df, ...) {
-  syms(tidyselect_names(.df, ...))
+tidyselect_syms <- function(.df, ..., .allow_rename = TRUE) {
+  syms(tidyselect_names(.df, ..., .allow_rename = .allow_rename))
 }
